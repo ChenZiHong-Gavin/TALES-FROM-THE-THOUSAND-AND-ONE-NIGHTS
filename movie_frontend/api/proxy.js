f06f740b0127dc920701f11b2dd2aff6aa0ed608
chenzihong-gavin <522023320011@smail.nju.edu.cn>
2023-12-20 02:41:20 +0800

    fix: 修改代理

--- movie_frontend/api/proxy.js
+++ movie_frontend/api/proxy.js
@@ -13,7 +13,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/backend`
       // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-      '^/api/': '/api/'
+      '^/proxy/api/': '/api/'
     }
   })(req, res)
 }
