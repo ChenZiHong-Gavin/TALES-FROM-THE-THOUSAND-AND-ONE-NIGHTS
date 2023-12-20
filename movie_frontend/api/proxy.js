8c87a7a04b67bbf0053944a717940b7a5e14df93
chenzihong-gavin <522023320011@smail.nju.edu.cn>
2023-12-20 09:56:47 +0800

    fix: 修改拼写

--- movie_frontend/api/proxy.js
+++ movie_frontend/api/proxy.js
@@ -3,13 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = ''
 
-  cnosole.log(req.url)
+  console.log(req.url)
 
   // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  // if (req.url.startsWith('/backend')) {
-  //   target = 'http://backend-api.com'
-  // }
   // 如果url中有/backend
   if (req.url.includes('/backend')) {
     target = 'http://47.103.127.114:8080'
