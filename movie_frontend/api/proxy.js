bfe65911304d63d831d7bce37cf4cfe575ccc523
chenzihong-gavin <522023320011@smail.nju.edu.cn>
2023-12-20 09:25:31 +0800

    fix: 修复代理

--- movie_frontend/api/proxy.js
+++ movie_frontend/api/proxy.js
@@ -1,10 +1,17 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
+  let target = ''
 
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  const target = 'http://47.103.127.114:8080'
+  // if (req.url.startsWith('/backend')) {
+  //   target = 'http://backend-api.com'
+  // }
+  // 如果url中有/backend
+  if (req.url.includes('/backend')) {
+    target = 'http://47.103.127.114:8080'
+  }
 
   // 创建代理对象并转发请求
   createProxyMiddleware({
@@ -13,8 +20,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/backend`
       // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-      '^/proxy/api/': '/api/'
+      '^/backend/': '/'
     }
   })(req, res)
 }
-
